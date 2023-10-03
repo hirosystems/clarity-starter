51b010f9186e3047c6d1238c2540af73152ccc9e
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2023-10-03 15:58:32 +0200

    chore: upgrade dependencies

--- tests/counter.test.ts
+++ tests/counter.test.ts
@@ -1,12 +1,6 @@
-import { initVM, tx } from "@hirosystems/clarinet-sdk";
+import { tx } from "@hirosystems/clarinet-sdk";
 import { Cl, UIntCV } from "@stacks/transactions";
-import { beforeEach, describe, expect, it } from "vitest";
-
-// By default, the VM is reinitialized before each test and keeps the same state.
-// Calling `initVM` in `beforeEach` allows to refresh the state before each test.
-beforeEach(async () => {
-  await initVM();
-});
+import { describe, expect, it } from "vitest";
 
 const accounts = vm.getAccounts();
 const address1 = accounts.get("wallet_1")!;
@@ -26,27 +20,8 @@ describe("test get counter", () => {
   });
 });
 
-describe("test get counter at block height", () => {
-  it("ensures <get-count> send the counter value", () => {
-    vm.callPublicFn("counter", "increment", [], address1);
-    vm.callPublicFn("counter", "increment", [], address1);
-
-    const atBlock1 = vm.callReadOnlyFn("counter", "get-count-at-block", [Cl.uint(1)], address2);
-    expect(atBlock1.result).toBeOk(Cl.uint(0));
-    const atBlock2 = vm.callReadOnlyFn("counter", "get-count-at-block", [Cl.uint(2)], address1);
-    expect(atBlock2.result).toBeOk(Cl.uint(1));
-  });
-
-  // we can actualy get the values in two ways
-  it("ensures the counter variable hold the right value", () => {
-    const counter = vm.getDataVar("counter", "count");
-    expect(counter).toBeUint(0);
-  });
-});
-
 describe("test <increment>", () => {
   it("ensures <increment> adds 1", () => {
-    ``;
     const { result } = vm.callPublicFn("counter", "increment", [], address1);
     expect(result).toBeOk(Cl.bool(true));
 
@@ -90,5 +65,23 @@ describe("test <add>", () => {
   it("ensures <add> throws an error if n is too low", () => {
     const { result } = vm.callPublicFn("counter", "add", [Cl.uint(1)], address1);
     expect(result).toBeErr(Cl.uint(1002));
+  });
+});
+
+describe("test get counter at block height", () => {
+  it("ensures <get-count> send the counter value", () => {
+    vm.callPublicFn("counter", "increment", [], address1);
+    vm.callPublicFn("counter", "increment", [], address1);
+
+    const atBlock1 = vm.callReadOnlyFn("counter", "get-count-at-block", [Cl.uint(1)], address2);
+    expect(atBlock1.result).toBeOk(Cl.uint(0));
+    const atBlock2 = vm.callReadOnlyFn("counter", "get-count-at-block", [Cl.uint(2)], address1);
+    expect(atBlock2.result).toBeOk(Cl.uint(1));
+  });
+
+  // we can actualy get the values in two ways
+  it("ensures the counter variable hold the right value", () => {
+    const counter = vm.getDataVar("counter", "count");
+    expect(counter).toBeUint(0);
   });
 });
